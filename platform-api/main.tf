155cd78683ca91d7d47803b685a6051f723e94c5
BerndWessels <wesselsbernd@gmail.com>
2017-05-02 08:04:53 +1200

    Cleanup.

--- platform-api/main.tf
+++ platform-api/main.tf
@@ -10,3 +10,21 @@ resource "aws_api_gateway_rest_api" "platform_api" {
   name = "platform_api_2"
   description = "Platform API 2"
 }
+
+/**
+ * REST API Deployment for the Platform.
+ */
+resource "aws_api_gateway_deployment" "platform_api" {
+  rest_api_id = "${aws_api_gateway_rest_api.platform_api.id}"
+  stage_name = "prod"
+  depends_on = ["null_resource.ready_for_deployment"]
+}
+
+/**
+ * Dummy resource to capture external dependencies.
+ */
+resource "null_resource" "ready_for_deployment" {
+  triggers {
+    dummy = "${var.ready_for_deployment}"
+  }
+}
---
+++ platform-api/variables.tf
@@ -0,0 +1,4 @@
+variable "ready_for_deployment" {
+  type = "list"
+  description = "This is an indicator that signals that the API is now ready to be deployed."
+}
