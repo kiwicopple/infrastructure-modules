44044158a1419e17a0efe90c00ebbed77c9bd2e9
BerndWessels <wesselsbernd@gmail.com>
2017-05-15 15:48:16 +1200

    Added platform-distribution and aws-s3-website.

--- aws-s3-website/outputs.tf
+++ aws-s3-website/outputs.tf
@@ -1,3 +1,6 @@
+output "bucket_domain_name" {
+  value = "${aws_s3_bucket.aws_s3_website.bucket_domain_name}"
+}
 output "website_endpoint" {
   value = "${aws_s3_bucket.aws_s3_website.website_endpoint}"
 }
