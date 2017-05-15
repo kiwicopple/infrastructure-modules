b086a118f86c66e7d3deb61ec199bb57a7d78455
BerndWessels <wesselsbernd@gmail.com>
2017-05-16 10:41:17 +1200

    Added platform-distribution and aws-s3-website.

--- platform-api/outputs.tf
+++ platform-api/outputs.tf
@@ -4,6 +4,9 @@ output "deployment_id" {
 output "deployment_invoke_url" {
   value = "${aws_api_gateway_deployment.platform_api.invoke_url}"
 }
+output "deployment_execution_arn" {
+  value = "${aws_api_gateway_deployment.platform_api.execution_arn}"
+}
 output "rest_api_id" {
   value = "${aws_api_gateway_rest_api.platform_api.id}"
 }
