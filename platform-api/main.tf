7c57da4ca8c87bc1ee36efa7036b0794ca9ae2ab
BerndWessels <wesselsbernd@gmail.com>
2017-05-16 14:57:12 +1200

    Added platform-distribution and aws-s3-website.

--- platform-api/main.tf
+++ platform-api/main.tf
@@ -19,7 +19,40 @@ resource "aws_api_gateway_deployment" "platform_api" {
   rest_api_id = "${aws_api_gateway_rest_api.platform_api.id}"
   stage_name = "${var.stage_name}"
   stage_description = "${var.stage_description}"
-  depends_on = ["aws_api_gateway_rest_api.platform_api"]
+  depends_on = ["aws_api_gateway_rest_api.platform_api", "module.platform_api_website", "module.platform_api_graphql"]
+}
+
+/**
+ * Create the Website endpoint for the Platform API.
+ */
+module "platform_api_website" {
+  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//aws-serverless-express"
+  region = "${var.region}"
+  filename = "${var.website_lambda_filename}"
+  function_name = "platform-api-website"
+  role_arn = "${var.website_lambda_role_arn}"
+  handler = "index.handler"
+  runtime = "nodejs6.10"
+  rest_api_id = "${aws_api_gateway_rest_api.platform_api.id}"
+  rest_api_parent_resource_id = "${aws_api_gateway_rest_api.platform_api.root_resource_id}"
+  create_child_resource = false
+}
+
+/**
+ * Create the GraphQL endpoint for the Platform API.
+ */
+module "platform_api_graphql" {
+  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//aws-serverless-express"
+  region = "${var.region}"
+  filename = "${var.graphql_lambda_filename}"
+  function_name = "platform-api-graphql"
+  role_arn = "${var.graphql_lambda_role_arn}"
+  handler = "index.handler"
+  runtime = "nodejs6.10"
+  rest_api_id = "${aws_api_gateway_rest_api.platform_api.id}"
+  rest_api_parent_resource_id = "${aws_api_gateway_rest_api.platform_api.root_resource_id}"
+  create_child_resource = true
+  child_resource_path = "graphql"
 }
 
 /**
@@ -40,30 +73,30 @@ resource "aws_api_gateway_deployment" "platform_api" {
 //  rest_api_parent_resource_id = "${aws_api_gateway_rest_api.platform_api.root_resource_id}"
 //}
 
-module "serverless_express_endpoint" {
-  count = 2
-  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//aws-serverless-express"
-  region = "${var.region}"
-  filename = "${lookup(var.serverless_express_endpoints[count.index], "filename")}"
-  function_name = "${lookup(var.serverless_express_endpoints[count.index], "function_name")}"
-  role_arn = "${lookup(var.serverless_express_endpoints[count.index], "role_arn")}"
-  handler = "${lookup(var.serverless_express_endpoints[count.index], "handler")}"
-  runtime = "${lookup(var.serverless_express_endpoints[count.index], "runtime")}"
-  create_child_resource = "${lookup(var.serverless_express_endpoints[count.index], "create_child_resource")}"
-  child_resource_path = "${lookup(var.serverless_express_endpoints[count.index], "child_resource_path")}"
-  rest_api_id = "${aws_api_gateway_rest_api.platform_api.id}"
-  rest_api_parent_resource_id = "${aws_api_gateway_rest_api.platform_api.root_resource_id}"
-}
-
-resource "null_resource" "serverless_express" {
-  count = 2
-  triggers {
-    filename = "${lookup(var.serverless_express_endpoints[count.index], "filename")}"
-    function_name = "${lookup(var.serverless_express_endpoints[count.index], "function_name")}"
-    role_arn = "${lookup(var.serverless_express_endpoints[count.index], "role_arn")}"
-    handler = "${lookup(var.serverless_express_endpoints[count.index], "handler")}"
-    runtime = "${lookup(var.serverless_express_endpoints[count.index], "runtime")}"
-    create_child_resource = "${lookup(var.serverless_express_endpoints[count.index], "create_child_resource")}"
-    child_resource_path = "${lookup(var.serverless_express_endpoints[count.index], "child_resource_path")}"
-  }
-}
+//module "serverless_express_endpoint" {
+//  count = 2
+//  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//aws-serverless-express"
+//  region = "${var.region}"
+//  filename = "${lookup(var.serverless_express_endpoints[count.index], "filename")}"
+//  function_name = "${lookup(var.serverless_express_endpoints[count.index], "function_name")}"
+//  role_arn = "${lookup(var.serverless_express_endpoints[count.index], "role_arn")}"
+//  handler = "${lookup(var.serverless_express_endpoints[count.index], "handler")}"
+//  runtime = "${lookup(var.serverless_express_endpoints[count.index], "runtime")}"
+//  create_child_resource = "${lookup(var.serverless_express_endpoints[count.index], "create_child_resource")}"
+//  child_resource_path = "${lookup(var.serverless_express_endpoints[count.index], "child_resource_path")}"
+//  rest_api_id = "${aws_api_gateway_rest_api.platform_api.id}"
+//  rest_api_parent_resource_id = "${aws_api_gateway_rest_api.platform_api.root_resource_id}"
+//}
+//
+//resource "null_resource" "serverless_express" {
+//  count = 2
+//  triggers {
+//    filename = "${lookup(var.serverless_express_endpoints[count.index], "filename")}"
+//    function_name = "${lookup(var.serverless_express_endpoints[count.index], "function_name")}"
+//    role_arn = "${lookup(var.serverless_express_endpoints[count.index], "role_arn")}"
+//    handler = "${lookup(var.serverless_express_endpoints[count.index], "handler")}"
+//    runtime = "${lookup(var.serverless_express_endpoints[count.index], "runtime")}"
+//    create_child_resource = "${lookup(var.serverless_express_endpoints[count.index], "create_child_resource")}"
+//    child_resource_path = "${lookup(var.serverless_express_endpoints[count.index], "child_resource_path")}"
+//  }
+//}
--- platform-api/variables.tf
+++ platform-api/variables.tf
@@ -10,7 +10,19 @@ variable "stage_description" {
   type = "string"
   description = "This is also an indicator that signals that the API is now ready to be deployed."
 }
-variable "serverless_express_endpoints" {
-  type = "list"
-  description = ""
+variable "website_lambda_filename" {
+  type = "string"
+  description = "The full path and filename to the lambda function's zip file."
+}
+variable "website_lambda_role_arn" {
+  type = "string"
+  description = "The IAM role ARN for the lambda function."
+}
+variable "graphql_lambda_filename" {
+  type = "string"
+  description = "The full path and filename to the lambda function's zip file."
+}
+variable "graphql_lambda_role_arn" {
+  type = "string"
+  description = "The IAM role ARN for the lambda function."
 }
