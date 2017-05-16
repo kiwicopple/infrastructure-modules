299b641feeefda2fa51dcc66cc86241f822abfd9
BerndWessels <wesselsbernd@gmail.com>
2017-05-16 14:02:23 +1200

    Added platform-distribution and aws-s3-website.

--- platform-api/main.tf
+++ platform-api/main.tf
@@ -22,33 +22,33 @@ resource "aws_api_gateway_deployment" "platform_api" {
   depends_on = ["aws_api_gateway_rest_api.platform_api"]
 }
 
-/**
- * Create the Website endpoint for the Platform API.
- */
-module "serverless_express_endpoint" {
-  count = 2
-  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//aws-serverless-express"
-  region = "${var.region}"
-  filename = "${null_resource.serverless_express[count.index].filename}"
-  function_name = "${null_resource.serverless_express[count.index].function_name}"
-  role_arn = "${null_resource.serverless_express[count.index].role_arn}"
-  handler = "${null_resource.serverless_express[count.index].handler}"
-  runtime = "${null_resource.serverless_express[count.index].runtime}"
-  create_child_resource = "${null_resource.serverless_express[count.index].create_child_resource}"
-  child_resource_path = "${null_resource.serverless_express[count.index].child_resource_path}"
-  rest_api_id = "${aws_api_gateway_rest_api.platform_api.id}"
-  rest_api_parent_resource_id = "${aws_api_gateway_rest_api.platform_api.root_resource_id}"
-}
+///**
+// * Create the Website endpoint for the Platform API.
+// */
+//module "serverless_express_endpoint" {
+//  count = 2
+//  source = "git::git@github.com:wessels-nz/infrastructure-modules.git//aws-serverless-express"
+//  region = "${var.region}"
+//  filename = "${null_resource.serverless_express[count.index].filename}"
+//  function_name = "${null_resource.serverless_express[count.index].function_name}"
+//  role_arn = "${null_resource.serverless_express[count.index].role_arn}"
+//  handler = "${null_resource.serverless_express[count.index].handler}"
+//  runtime = "${null_resource.serverless_express[count.index].runtime}"
+//  create_child_resource = "${null_resource.serverless_express[count.index].create_child_resource}"
+//  child_resource_path = "${null_resource.serverless_express[count.index].child_resource_path}"
+//  rest_api_id = "${aws_api_gateway_rest_api.platform_api.id}"
+//  rest_api_parent_resource_id = "${aws_api_gateway_rest_api.platform_api.root_resource_id}"
+//}
 
 resource "null_resource" "serverless_express" {
   count = 2
   triggers {
-    filename = "${lookup(var.serverless_express_endpoints[count.index], "filename")}"
-    function_name = "${lookup(var.serverless_express_endpoints[count.index], "function_name")}"
-    role_arn = "${lookup(var.serverless_express_endpoints[count.index], "role_arn")}"
-    handler = "${lookup(var.serverless_express_endpoints[count.index], "handler")}"
-    runtime = "${lookup(var.serverless_express_endpoints[count.index], "runtime")}"
-    create_child_resource = "${lookup(var.serverless_express_endpoints[count.index], "create_child_resource")}"
-    child_resource_path = "${lookup(var.serverless_express_endpoints[count.index], "child_resource_path")}"
+    filename = "${var.serverless_express_endpoints.*.filename}"
+    function_name = "${var.serverless_express_endpoints.*.function_name}"
+    role_arn = "${var.serverless_express_endpoints.*.role_arn}"
+    handler = "${var.serverless_express_endpoints.*.handler}"
+    runtime = "${var.serverless_express_endpoints.*.runtime}"
+    create_child_resource = "${var.serverless_express_endpoints.*.create_child_resource}"
+    child_resource_path = "${var.serverless_express_endpoints.*.child_resource_path}"
   }
 }
