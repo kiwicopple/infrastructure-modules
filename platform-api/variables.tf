d7f52a4e4b90efc2406efc8acbbd8e6cdf59c9e4
BerndWessels <wesselsbernd@gmail.com>
2017-05-16 13:42:03 +1200

    Added platform-distribution and aws-s3-website.

--- platform-api/variables.tf
+++ platform-api/variables.tf
@@ -1,14 +1,16 @@
+variable "region" {
+  type = "string"
+  description = "The AWS Region for the lambda function."
+}
 variable "stage_name" {
   type = "string"
   description = "The state name for the API deployment."
 }
-
 variable "stage_description" {
   type = "string"
   description = "This is also an indicator that signals that the API is now ready to be deployed."
 }
-
-variable "serverless_express_endpoint" {
+variable "serverless_express_endpoints" {
   type = "list"
   description = ""
 }
